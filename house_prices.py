6555d40b9a4a8cbcdf5635ec19d00ef6b3c7db03
Andersen <mizio.andersen@gmail.com>
2017-02-06 15:50:31 +0100

    Implemented imputation method (MICE) for numerical value features as part of cleaning data.

--- house_prices.py
+++ house_prices.py
@@ -7,8 +7,10 @@ import pandas as pd
 # matplotlib.use('TkAgg')
 import pylab as plt
 from fancyimpute import MICE
+
 import random
 from sklearn.model_selection import cross_val_score
+# from sklearn.ensemble import RandomForestClassifier
 
 class HousePrices(object):
     def __init__(self):
@@ -24,23 +26,30 @@ class HousePrices(object):
         square_meter_per_square_feet = 0.3048**2
         return area*square_meter_per_square_feet
 
+    def extract_numerical_features(self, df):
+        return df.select_dtypes(include=[np.number])
 
     def clean_data(self, df):
-        # Drop all nulls and non-value type features
+        # Drop all non numeric type features
         if any('SalePrice' == df.columns.values):
-            # Additionally for training data, drop all rows with nulls
-            df_cleaned = df.select_dtypes(include=[np.number]).dropna()
+            drop_non_or_nan_rows = 0
+            if drop_non_or_nan_rows:
+                # Additionally for training data, drop all rows with nulls
+                df_cleaned = self.extract_numerical_features(df).dropna()
+            else:
+                df_cleaned = self.extract_numerical_features(df)
         else:
-            df_cleaned = df.select_dtypes(include=[np.number])
+            df_cleaned = self.extract_numerical_features(df)
 
+        # Imputation using MICE
+        df_cleaned = self.estimate_by_mice(df_cleaned)
         return df_cleaned
-        # return df
 
     def feature_engineering(self, df):
         # df['LotAreaSquareMeters'] = df['LotArea']
-        #
         # df['LotAreaSquareMeters'] = self.square_feet_to_meters(df.LotAreaSquareMeters.values)
         pass
+        # return df_feature_engineered
 
 
     def drop_variable(self, df):
@@ -54,10 +63,27 @@ class HousePrices(object):
     def prepare_data_random_forest(self, df):
         df = self.clean_data(df)
         self.feature_engineering(df)
-        df = self.drop_variable(df)
+        # df = self.drop_variable(df)
         # self.feature_scaling(df)
         return df
 
+    def indices_with_non_or_nan_values(self, df, axis=1):
+        agg_axis = 1 - axis
+        agg_obj = df[:][:]
+        count = agg_obj.count(axis=agg_axis)
+        mask = count == len(agg_obj._get_axis(agg_axis))
+        return mask[mask == 0]
+        # return mask[mask == 0].axes
+
+    # Todo: imputation for the 11 columns targeted by the test data
+    # Todo: estimate the missing values in numeric feature columns using mice and all numeric columns
+    def estimate_by_mice(self, df):
+        df_estimated_var = df[:][:]
+        random.seed(129)
+        mice = MICE()  #model=RandomForestClassifier(n_estimators=100))
+        res = mice.complete(df.values)
+        df_estimated_var[df.columns] = res[:][:]
+        return df_estimated_var
 
 
 def main():
@@ -78,6 +104,7 @@ def main():
     df_publ = house_prices.df[:][:]
     df_test_publ = house_prices.df_test[:][:]
 
+
     df = house_prices.prepare_data_random_forest(df_publ)
     print '\n TRAINING DATA:----------------------------------------------- \n'
     print df.head(3)
@@ -96,12 +123,33 @@ def main():
     print '\n'
 
 
-    # Todo: return list of non-null columns in test data
-    non_null_feature_columns = df_test.columns.values
-    df = df[np.append(non_null_feature_columns, 'SalePrice')]
-    print df.info()
+    # Todo: return list of columns without non/nan values in test data
+    # non_null_feature_columns = df_test.columns.values
+    # df = df[np.append(non_null_feature_columns, 'SalePrice')]
+    # print df.info()
     train_data = df.values
     test_data = df_test.values
+
+    # Print numeric feature columns with none/nan in test data
+    print '\nColumns in train data with non/nan values:\n'
+    df_publ_cleaned = house_prices.extract_numerical_features(df_publ)
+    print house_prices.indices_with_non_or_nan_values(df_publ_cleaned)
+
+
+    # Print numeric feature columns with none/nan in test data
+    print '\nColumns in test data with non/nan values:\n'
+    df_test_publ_cleaned = house_prices.extract_numerical_features(df_test_publ)
+    print house_prices.indices_with_non_or_nan_values(df_test_publ_cleaned)
+
+    # Imputation method applied to numeric columns in test data with non/nan values
+    df_imputed = house_prices.estimate_by_mice(df_publ_cleaned)
+    print("Training set missing values")
+    print(df_imputed.isnull().sum())
+    df_test_imputed = house_prices.estimate_by_mice(df_test_publ_cleaned)
+    print("Testing set missing values")
+    print(df_test_imputed.isnull().sum())
+
+
 
 
     ''' Explore data '''
@@ -168,22 +216,14 @@ def main():
 
     ''' Submission '''
     save_path = '/home/user/Documents/Kaggle/HousePrices/submission/'
-    # dfBestScore = pd.read_csv(''.join(save_path, 'submissionTitanic.csv'), header=0)
+    # Submission requires a csv file with Id and SalePrice columns.
+    dfBestScore = pd.read_csv(''.join([save_path, 'submission_house_prices.csv']), header=0)
 
     # We do not expect all to be equal since the learned model differs from time to time.
     # print (dfBestScore.values[0::, 1::].ravel() == output.astype(int))
     # print np.array_equal(dfBestScore.values[0::, 1::].ravel(), output.astype(int))  # But they are almost never all equal
     submission = pd.DataFrame({'Id': Id_df_test, 'SalePrice': output})
     submission.to_csv(''.join([save_path, 'submission_house_prices.csv']), index=False)
-
-    # Using numpy instead of Pandas for submit
-    # submitArray = np.array([passengerId_df_test.values, output]).T
-    # np.savetxt(''.join([savePath, 'submissionTitanic.csv']), submitArray, delimiter=',')
-    # plt.plot(np.array(passengerId_df_test), np.array(output), 'o')
-    # plt.show()
-
-
-
 
 if __name__ == '__main__':
     main()
